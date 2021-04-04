ff0bf3e736fc73917387fd102561285501a8a52f
Jady Liu <jady.liu@dowjones.com>
2021-04-04 12:13:11 -0400

    test

--- tf_aws_userdata/main.tf
+++ tf_aws_userdata/main.tf
@@ -11,6 +11,7 @@ data "template_file" "linux_userdata" {
     chef_cookbook         = "${var.chef_cookbook}"
     chef_cookbook_version = "${var.chef_cookbook_version}"
     s3_bucket             = "${var.s3_bucket}"
+    region                = "${var.region}"
   }
 }
 
